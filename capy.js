b90e4e0fdd6f771f2e5a93f645cbfb6af5934ed3
pri-r <137687143+pri-r@users.noreply.github.com>
2023-12-24 12:10:26 +1100

    Add files via upload

---
+++ capy.js
@@ -0,0 +1,94 @@
+let currCapyTile;
+let currRacoonTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame () {
+    for (let i = 0; i < 9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+
+    while (score > 200) {
+        setInterval(setCapy, 150); 
+        setInterval(setRacoon, 200); 
+    }
+
+    while (score > 150) {
+        setInterval(setCapy, 400); 
+        setInterval(setRacoon, 600); 
+    }
+
+    while (score > 100) {
+        setInterval(setCapy, 500); 
+        setInterval(setRacoon, 1000); 
+    }
+
+    setInterval(setCapy, 1000); 
+    setInterval(setRacoon, 2000); 
+}
+
+function getRandom () {
+    let num = Math.floor(Math.random() * 9); 
+    return num.toString();
+}
+
+function setCapy () {
+    if (gameOver) {
+        return;
+    }
+
+    if (currCapyTile) {
+        currCapyTile.innerHTML = "";
+    }
+
+    let capy = document.createElement("img"); 
+    capy.src = "../../Downloads/capybara.png";
+
+    let number = getRandom();
+    if (currRacoonTile && currRacoonTile.id == number) {
+        return;
+    }
+    currCapyTile = document.getElementById(number);
+    currCapyTile.appendChild(capy);
+}
+
+function setRacoon () {
+    if (gameOver) {
+        return;
+    }
+    if (currRacoonTile) {
+        currRacoonTile.innerHTML = "";
+    }
+
+    let racoon = document.createElement("img"); 
+    racoon.src = "./CAPY/racoon.png";
+    
+    let num = getRandom();
+    if (currCapyTile && currCapyTile.id == num) {
+        return;
+    }
+
+    currRacoonTile = document.getElementById(num);
+    currRacoonTile.appendChild(racoon);
+}
+
+function selectTile () {
+    if (gameOver) {
+        return;
+    }
+    
+    if (this == currCapyTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    } else if (this == currRacoonTile) {
+        document.getElementById("score").innerText = "OOPS, YOU LOST: " + score.toString();
+        gameOver = true;
+    }
+}
